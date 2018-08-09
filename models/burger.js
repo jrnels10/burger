6c8e83eb36ea7be92cfe440ddd57af094bc01921
Jacob <jrnels10@gmail.com>
2018-08-08 19:03:07 -0700

    game finished

--- models/burger.js
+++ models/burger.js
@@ -4,12 +4,23 @@ const orm = require("../config/orm.js");
 //burger specific input from the ORM
 
 const burger = {
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(res){
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
             cb(res);
             // console.log(res)
         })
-    }
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+      }
 }
 
 // console.log("burgers.js page: " + burger.selectAll())
